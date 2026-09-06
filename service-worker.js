8a95310eafe609d021d22edebab7aa789d2fd2cc
Luis200701 <luis2007.lm@gmail.com>
2026-09-06 11:45:55 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // eine leere Seite zu zeigen. Runden-Daten von Supabase laufen weiterhin
 // normal ueber das Netz, die werden hier bewusst nicht angefasst.
 
-const CACHE_NAME = 'pocketcaddy-v5';
+const CACHE_NAME = 'pocketcaddy-v11';
 const APP_SHELL = [
   './',
   './index.html',
